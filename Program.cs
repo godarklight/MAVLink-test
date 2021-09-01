8ebbd51e0404ea73ffe080b203a7f97619c84999
Christopher Andrews <godarklight@gmail.com>
2021-09-02 09:10:09 +1000

    Change to .NET Core, start processing messages

--- Program.cs
+++ Program.cs
@@ -11,25 +11,50 @@ namespace TcpServer
     {
         static void Main(string[] args)
         {
-            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 1234);
+            MAVLink mav = new MAVLink();
+            byte[] buffer = new byte[263];
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 5760);
             TcpListener listener = new TcpListener(ep);
             listener.Start();
 
             Console.WriteLine(@"Address: " + ep.Address + "\t Port: " + ep.Port);
+            TcpClient sender = listener.AcceptTcpClient();
+            Console.WriteLine("Connected");
 
-            // Run the loop continously; this is the server.  
+            // Run the loop continously; this is the server.
+            bool readingHeader = true;
+            int bytesLeft = 8;
+            int readPos = 0;
             while (true)
             {
-                const int bytesize = 263;
-                byte[] buffer = new byte[bytesize];
+                int bytesRead = sender.GetStream().Read(buffer, readPos, bytesLeft);
+                readPos += bytesRead;
+                bytesLeft -= bytesRead;
+                if (bytesRead == 0)
+                {
+                    //Disconnect
+                    return;
+                }
+                if (bytesLeft == 0)
+                {
+                    if (readingHeader)
+                    {
+                        readingHeader = false;
+                        bytesLeft = buffer[1];
+                    }
+                    else
+                    {
+                        //Process buffer here
+                        MAVLink.MAVLinkMessage mlm = new MAVLink.MAVLinkMessage(buffer);
+                        string hexString = BitConverter.ToString(buffer, 0, readPos);
+                        Console.WriteLine(mlm.msgtypename + ": " + hexString.Replace('-', ' '));
+                        Console.WriteLine("===");
 
-                var sender = listener.AcceptTcpClient();
-                sender.GetStream().Read(buffer, 0, bytesize);
-
-                // Trimming the bufferr before dumping it to the console
-                byte[] bufferTrimmed = MavlinkUtil.trim_payload(ref buffer);
-                string hexString = BitConverter.ToString(bufferTrimmed);
-                Console.WriteLine(hexString.Replace('-', ' '));
+                        readingHeader = true;
+                        readPos = 0;
+                        bytesLeft = 8;
+                    }
+                }
 
                 // I have no clue on how to make it into a MAVLink packet
                 //var packet = MavlinkUtil.ByteArrayToStructure
